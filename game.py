f57d5aed7c641c4475a57ef2868bd4ff8fe0b306
Smick <smickminecraft@gmail.com>
2024-08-28 16:34:36 -0400

    Juego Base

--- game.py
+++ game.py
@@ -1,3 +1,20 @@
 import random
+import time
 
 number_random = random.randint(1, 100)
+
+print("Generando numero")
+for i in range(3):
+    time.sleep(1)
+    print(".")
+    
+print("Numero Generado exitosamente!")
+print(number_random)
+
+while True:
+    number_by_user = input("Ingrese un numero del 1 al 100\n---> ")
+    if number_by_user == str(number_random):
+        print("¡Hurra! Has adivinado el numero")
+        break
+    else:
+        print("Intentelo de nuevo\n\n")
